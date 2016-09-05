13af22d28bca504e29484b94d0d9416a7213771b
rasho22 <ab_rasho@yahoo.fr>
2016-09-06 01:55:37 +0200

    création page js et amélioration html et css

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
 $(document).ready(function () {
-  
+
+	// le  carrousel
+   setInterval(function(){
+      $("#carrousel ul").animate({marginLeft:-350},800,function(){
+        $(this).css({marginLeft:0}).find("li:last").after($(this).find("li:first"));
+         })
+      }, 5000);
+
+   //animation réseaux socio
+   $( "#button3" ).click(function() {
+  		$( "#rezo" ).animate({
+    		opacity: 0.25,
+    		left: "+=50",
+    		height: "toggle"
+ 		 }, 5000, function() {
+  		});
+	});
+
+   // animation exemples des sites
+	$( "#button2" ).click(function() {
+		$( "#demo" ).animate({
+		    width: [ "toggle", "swing" ],
+		    height: [ "toggle", "swing" ],
+		    opacity: "toggle"
+		  }, 5000, "linear", function() {
+		   
+		});
+	});
+
+// animation cv
+$( "#button1" ).click(function() {
+  		$( "#cv" ).animate({
+    		opacity: 0.25,
+    		left: "+=50",
+    		height: "toggle"
+ 		 }, 5000, function() {
+  		});
+	});
+
 });
---
+++ portfolio.php
@@ -0,0 +1,91 @@
+<html>
+<head>
+	<title>Portfolio AbdulRahman RASHO</title>
+	<meta charset="utf-8">
+	  	 <meta name="viewport" content="width=device-width, target-densitydpi=device-dpi"/>
+	 	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	 	<link rel="stylesheet" type="text/css" href="style.css">
+		 <script type="text/javascript"  href="js/jquery.min.js" ></script>
+	       <script type="text/javascript" href="js/script.js"></script>
+</head>
+<body>
+	<div class="container">
+		<!--navbar-->
+		<div class="col-md-12 col-sm-12 col-xs-12">
+			<nav class="navbar navbar-inverse navbar-fixed-top">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#"> <h1>Portfolio</h1> <br/>
+                            <h3>Abdul Rahman RASHO</h3>
+                        </a>
+                    </div>
+                    <ul class="nav navbar-nav">
+                    	<li><a href="#">Home</a></li>
+                        <li><a href="#">Bibliographie</a></li>
+                        <li><a href="#">Démonstration</a></li>
+                        <li><a href="#">Me conntacter</a></li>
+                    </ul>      
+            </nav>
+		</div>
+
+
+		<!--blok central-->
+		<div class="row">
+		<div class="col-md-12 col-sm-12 col-xs-12">
+			<div id="carrousel">
+				<ul> 
+                    <li><a href="#"><img src="img/challenge2.png"/></a></li>
+                    <li><a href="#"><img src="img/challenge4.png"/></a></li>
+                    <li><a href="#"><img src="img/challenge5.png"/></a></li>
+                    <li><a href="#"><img src="img/js_quiz.png"/></a></li>
+                    <li><a href="#"><img src="img/fedala.png"/></a></li>
+                    <li><a href="#"><img src="img/Mac-Gyver.png"/></a></li>
+                </ul>
+			</div>		
+		</div>
+		</div>
+
+		<!-- sous blockcentral -->
+		  <div id="blockcentral" class="row">
+		  	<div class="col-md-12 col-sm-12 col-xs-12" >
+            <div  class="col-md-4 col-sm-12 col-xs-12">
+                <div id="bibliographie">
+                	<a href="#"><img id="cv" src="img/moi.jpg"/></a><br>
+                	<button id="button1"type="button" class="btn btn-success btn-lg">Ma Bibliographie</button>
+                </div>
+                
+            
+            </div>
+
+            <div  class="col-md-4 col-sm-12 col-xs-12">
+                <div class="Démonstration">
+                	
+                	<a href="#"><img id="demo" src="img/fedala.png"/></a><br>
+                	<button id="button2" type="button" class="btn btn-success btn-lg"> Démonstration Sites Web  </button>
+                </div>
+              
+
+            </div>
+
+            <div class="col-md-4 col-sm-12 col-xs-12">
+                <div id="conntacte" >
+                	<a href="#"><img id="rezo" src="img/rézo.jpg"/></a><br>
+                	<button id="button3" type="button" class="btn btn-success btn-lg">Me conntacter</button>
+                </div>
+                
+
+            </div>
+         </div>            
+        </div>
+
+        <!--  fotter  -->
+        <div id="fotter" class="row">
+            <div  class="col-md-12 col-sm-12 col-xs-12">
+                <h5>Copyright Abdul Rahman RASHO © 2016 </h5>
+            </div>
+                     
+        </div>
+	</div>
+	<script src="js/jquery-2.2.2.min.js"></script>
+	    <script src="js/script.js"></script>
+</body>
+</html>
