d74d841b1991c1e86fca4f092362fbcc01c8fed2
rasho22 <ab_rasho@yahoo.fr>
2017-01-01 12:05:48 +0100

    merge

--- cv.php
+++ cv.php
@@ -1,65 +1,65 @@
-<!DOCTYPE html> 
-<html> 
-<head> 
-  <title>CV/Portfolio AbdulRahman RASHO</title> 
-</head> 
-<body> 
+<!DOCTYPE html>
+<html>
+<head>
+  <title>CV/Portfolio AbdulRahman RASHO</title>
+</head>
+<body>
 
-   <div class="container"> 
+   <div class="container">
      <?php include("navbar.php")
-       ?> 
-    <div id="leftblock"class="row"> 
- 
-      <!--left section--> 
-      <div id="" class="col-md-4 col-md-offset-1 sm-12 col-xs-12"> 
-        <div class="col-md-12 col-md-offset-2 sm-12 col-xs-12 imgcv"> 
-          <img src="img/monimage.jpeg"> 
-        </div>  
+       ?>
+    <div id="leftblock"class="row">
+
+      <!--left section-->
+      <div id="" class="col-md-4 col-md-offset-1 sm-12 col-xs-12">
+        <div class="col-md-12 col-md-offset-2 sm-12 col-xs-12 imgcv">
+          <img src="img/monimage.jpeg">
+        </div>
           <div class="col-md-12 sm-12 col-xs-12 adresse">
             <i class="fa fa-envelope" style="font-size: 30px"></i>  :
-            <p>3 rue Pierre-Henri TEITGEN</p> 
-            <p>35000 Rennes</p> 
-            <p>42ans</p> 
+            <p>3 rue Pierre-Henri TEITGEN</p>
+            <p>35000 Rennes</p>
+            <p>42ans</p>
             <p>rasho80@hotmail.com</p>
-          </div> 
-           
-        <div class="col-md-12 sm-12 col-xs-12 phrasecle"> 
-          <p> <em>"Actuellement en formation développeur web/mobile,  
-            au sein de Face Rennes Simplon. Je travaille sur les langages suivants: 
-             HTML5, CSS3, PHP, JavaScript, Mysql. 
-             J'envisage de travailler en tant que développeur web dans le bassin rennais. 
-             Auparavant j'étais dans l'enseignement supérieur à l'étranger,  
-             titulaire d'un diplôme national de docteur en psychologie, trilingue, diplôme ICDL,  
-             Maîtrise des logiciels de statistique genre SPSS, autonome, esprit d'équipe, bon relationel.."</em></p> 
-        </div> 
- 
-        <div class="col-md-12 sm-12 col-xs-12 cv"> 
-          <h3>INFORMATIQUE</h3> 
-            <ul> 
-              <li>Langages: HTML5, CSS3, JAVA SCRIPT, PHP, MYSQL, XML</li> 
-              <li>Autres: WORDPRESS, GITHUB</li> 
-              <li>framework: Jquery,... </li> 
-              <li>Certification: ICDL;</li> 
-              <li>Statistique : SPSS, PSPP, R.</li> 
+          </div>
+
+        <div class="col-md-12 sm-12 col-xs-12 phrasecle">
+          <p> <em>"Actuellement en formation développeur web/mobile,
+            au sein de Face Rennes Simplon. Je travaille sur les langages suivants:
+             HTML5, CSS3, PHP, JavaScript, Mysql.
+             J'envisage de travailler en tant que développeur web dans le bassin rennais.
+             Auparavant j'étais dans l'enseignement supérieur à l'étranger,
+             titulaire d'un diplôme national de docteur en psychologie, trilingue, diplôme ICDL,
+             Maîtrise des logiciels de statistique genre SPSS, autonome, esprit d'équipe, bon relationel.."</em></p>
+        </div>
+
+        <div class="col-md-12 sm-12 col-xs-12 cv">
+          <h3>INFORMATIQUE</h3>
+            <ul>
+              <li>Langages: HTML5, CSS3, JAVA SCRIPT, PHP, MYSQL, XML</li>
+              <li>Autres: WORDPRESS, GITHUB</li>
+              <li>framework: Jquery,... </li>
+              <li>Certification: ICDL;</li>
+              <li>Statistique : SPSS, PSPP, R.</li>
             </ul>
-        </div> 
- 
-      <div class="col-md-12 sm-12 col-xs-12 cv"> 
-        <h3>LANGUES </h3> 
-          <ul>  
-            <li>Kurde:maternelle </li> 
-            <li>Arabe:courant </li> 
-            <li>Anglais: bon niveau</li> 
-          </ul> 
-      </div> 
- 
-      <div class="col-md-12 sm-12 col-xs-12 cv"> 
-      <h3>CENTRES D’INTÉRÊT</h3> 
-        <ul> <li>Natation , voyage, échecs</li></ul> 
-      </div> 
-    </div> 
- 
-<!--section right--> 
+        </div>
+
+      <div class="col-md-12 sm-12 col-xs-12 cv">
+        <h3>LANGUES </h3>
+          <ul>
+            <li>Kurde:maternelle </li>
+            <li>Arabe:courant </li>
+            <li>Anglais: bon niveau</li>
+          </ul>
+      </div>
+
+      <div class="col-md-12 sm-12 col-xs-12 cv">
+      <h3>CENTRES D’INTÉRÊT</h3>
+        <ul> <li>Natation , voyage, échecs</li></ul>
+      </div>
+    </div>
+
+<!--section right-->
       <div id="rightblock" class="col-md-6 col-md-offset-1 sm-12 col-xs-12">
         <div class="col-md-12 sm-12 col-xs-12 nom">
           <h1 >Abdul Rahman RASHO</h1>
@@ -76,51 +76,51 @@
           <a href="https://www.linkedin.com/in/abdulrahman-rasho-7a85a568" title= "Suivez-moi sur linkedin">
             <img src= "img/linkedin.png"  width="32px" height="32px"/>
           </a>
-        </div> 
-        <div class="col-md-12 sm-12 col-xs-12 cv"> 
-        <h3>FORMATIONS ET DIPLÔMES</h3> 
-          <ul>  
-            <li>17/05/2016-17/01/2017:Formation développeur Web/Mobile (8mois), Code Académie/ Face Rennes;</li> 
-            <li>18/12/2009:Doctorat en Psychologie, Université Rennes2,Mention très honorable;</li> 
-            <li>2005: Master2 recherches, en Psychopathologie et Champs Cliniques, Université Rennes2;</li> 
-            <li>2003/2004: Diplôme de la langue Française, CIREFE, Université Rennes2;</li> 
-            <li>2002/2003: Diplôme de la langue Française, Institut des langues/université de Damas (Syrie);</li> 
-            <li>2001/2002: DES en Psychologie d’Information, Université de Damas (Syrie);</li> 
-            <li>1996/2000: Licence en Psychologie, (niveau Bac+4), Université de Damas (Syrie);</li> 
-            <li>1995: Baccalauréat Général, Section Lettres, Hasaka (Syrie).</li> 
-          </ul> 
+        </div>
+        <div class="col-md-12 sm-12 col-xs-12 cv">
+        <h3>FORMATIONS ET DIPLÔMES</h3>
+          <ul>
+            <li>17/05/2016-17/01/2017:Formation développeur Web/Mobile (8mois), Code Académie/ Face Rennes;</li>
+            <li>18/12/2009:Doctorat en Psychologie, Université Rennes2,Mention très honorable;</li>
+            <li>2005: Master2 recherches, en Psychopathologie et Champs Cliniques, Université Rennes2;</li>
+            <li>2003/2004: Diplôme de la langue Française, CIREFE, Université Rennes2;</li>
+            <li>2002/2003: Diplôme de la langue Française, Institut des langues/université de Damas (Syrie);</li>
+            <li>2001/2002: DES en Psychologie d’Information, Université de Damas (Syrie);</li>
+            <li>1996/2000: Licence en Psychologie, (niveau Bac+4), Université de Damas (Syrie);</li>
+            <li>1995: Baccalauréat Général, Section Lettres, Hasaka (Syrie).</li>
+          </ul>
       </div>
 
-      <div class="col-md-12 sm-12 col-xs-12 cv" > 
-      <h3>EXPÉRIENCES PROFESSIONNELLES</h3> 
-        <ul>  
-          <li>12/2015-04/2016 Employé polyvalent, SUPERASIA, Rennes ;</li> 
-          <li>05/2014-11/2014  Secours Populaire (bénévole), au sein groupe santé et orientation; </li> 
-          <li>09/2012-03/2014 Maître de conférence, Université de Zakho/(Kurdistan Irakien)  </li> 
-          <li>09/2010-08/2012 Maître de conférence, Université de Damas</li> 
-          <li>09/2000-08/2002 Assistant maître de conférence, Université de Damas</li> 
-        </ul> 
-      </div> 
- 
-      <div class="col-md-12 sm-12 col-xs-12 cv"> 
-        <h3>COMPÉTENCES</h3> 
-          <ul>  
-            <li>Appréhender les besoins clients; </li> 
-            <li>Rédiger les spécifications détaillées; </li> 
-            <li>Gestion du projet  de cahier  des charges à la mise en production; </li> 
-            <li>Déterminer les objectifs;</li> 
-            <li>Traiter et analyser des données par logiciels statistiques ;  </li> 
-            <li>Mes atouts : Polyvalent, bon relationnel, bonne capacité d’analyse et de synthèse,</li> 
-          </ul> 
+      <div class="col-md-12 sm-12 col-xs-12 cv" >
+      <h3>EXPÉRIENCES PROFESSIONNELLES</h3>
+        <ul>
+          <li>12/2015-04/2016 Employé polyvalent, SUPERASIA, Rennes ;</li>
+          <li>05/2014-11/2014  Secours Populaire (bénévole), au sein groupe santé et orientation; </li>
+          <li>09/2012-03/2014 Maître de conférences, Université de Zakho/(Kurdistan Irakien)  </li>
+          <li>09/2010-08/2012 Maître de conférences, Université de Damas</li>
+          <li>09/2000-08/2002 Assistant maître de conférences, Université de Damas</li> 
+        </ul>
       </div>
 
-      </div> 
-    </div> 
-      <!--  footer  --> 
+      <div class="col-md-12 sm-12 col-xs-12 cv">
+        <h3>COMPÉTENCES</h3>
+          <ul>
+            <li>Appréhender les besoins clients; </li>
+            <li>Rédiger les spécifications détaillées; </li>
+            <li>Gestion du projet  de cahier  des charges à la mise en production; </li>
+            <li>Déterminer les objectifs;</li>
+            <li>Traiter et analyser des données par logiciels statistiques ;  </li>
+            <li>Mes atouts : Polyvalent, bon relationnel, bonne capacité d’analyse et de synthèse,</li>
+          </ul>
+      </div>
+
+      </div>
+    </div>
+      <!--  footer  -->
   <?php include("footer.php")
    ?>
-</div> 
+</div>
 <script type="text/javascript" src="js/jquery-2.2.2.min.js"></script>
 <script type="text/javascript" src="js/script.js"></script>
-</body> 
+</body>
 </html>
